b0e02cd94b75af00d80fbfd3e8c221c156d117dd
John <johnpairat@gmail.com>
2024-05-20 16:29:58 +0800

    Completed exercise and passed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...items) {
+    newArray = []
+    for (const item of array) {
+        if (!items.includes(item)) {
+            newArray.push(item)
+        } 
+    } 
+    return newArray
 };
 
 // Do not edit below this line
