f1693616a71700d188fe1726f9e1c10cd896d594
John <johnpairat@gmail.com>
2024-05-21 11:07:14 +0800

    Completed exercise and passed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempFahr) {
+  return tempFahr === 32 ? 0 :
+    Number(((tempFahr - 32) * 5/9).toFixed(1))
+};
+  
+const convertToFahrenheit = function(tempCelc) {
+  return tempCelc === 0 ? 32 :
+    Number(((tempCelc * 9/5) + 32).toFixed(1))
 };
 
-const convertToFahrenheit = function() {
-};
 
 // Do not edit below this line
 module.exports = {
